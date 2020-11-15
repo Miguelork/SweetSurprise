904c5effda82903996f9f10f0ab9719ee15f14b1
Miguelangel <miguelants14@gmail.com>
2020-11-15 18:38:25 -0400

    crud product listo y a la orden

--- src/app/components/admieditproduct/admieditproduct.component.ts
+++ src/app/components/admieditproduct/admieditproduct.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product';
+import { CrudproductService } from 'src/app/services/crudproduct.service';
 
 @Component({
   selector: 'app-admieditproduct',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admieditproduct.component.scss']
 })
 export class AdmieditproductComponent implements OnInit {
+  @Input() editProducto:Product={
+    nombre:'',
+    descripcion:'',
+    categoria:'',
+    precio:0,
+    stock:0,
+    fabricante:'',
+    img1:null,
+    img2:null,
+    img3:null,
+  }
 
-  constructor() { }
+  constructor(private crudp: CrudproductService) { }
 
   ngOnInit(): void {
+  }
+
+  editar(producto){ 
+    this.editProducto = producto;
+  }
+
+  aggProductEditado(){
+    this.crudp.editarProducto(this.editProducto);
   }
 
 }
