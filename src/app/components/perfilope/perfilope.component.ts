d2a3f28bc13831736f9349db5437ee462b02b05f
kevin HernandezQ <kevin147444@gmail.com>
2020-12-10 01:44:12 -0400

    ordenes-cliente

--- src/app/components/perfilope/perfilope.component.ts
+++ src/app/components/perfilope/perfilope.component.ts
@@ -4,6 +4,9 @@ import { Router, RouterModule } from '@angular/router';
 import { OrdersService } from '../../services/order.service';
 import { Order } from '../../models/order';
 import { AuthService } from '../../auth/auth.service';
+import { Subject, combineLatest } from 'rxjs';
+import { AngularFirestore } from '@angular/fire/firestore';
+
 
 
 @Component({
@@ -12,26 +15,39 @@ import { AuthService } from '../../auth/auth.service';
   styleUrls: ['./perfilope.component.scss']
 })
 export class PerfilopeComponent implements OnInit {
+  isCollapsed = true;
+  orders: any
+  searchterm: string = "";
+  startAt = new Subject();
+  endAt = new Subject();
+  startObs = this.startAt.asObservable();
+  endObs = this.endAt.asObservable();
+  addScript: boolean = false;
+  selectedOrder: any;
+  paypalLoad: boolean = true;
+  constructor(
+    private ordersService: OrdersService,
+    private auth: AuthService, 
+    private afs: AngularFirestore,
+    private orderService: OrdersService,) {}
 
- 
-  productos: any;
-  User_id: string;
-
-  constructor(private crudp: OrdersService, private router:Router, public auth: AuthService) {
-    this.crudp.getorders()
-    this.productos = this.crudp;
-  }
-uctor() { }
-
-  ngOnInit(): void {
-    this.auth.User.subscribe(user => {
-      if(user){
-          this.crudp.myordersRef(user.uid).subscribe(Cart => {
-            this.productos = Cart.payload.data();
-            this.User_id = user.uid; 
-          })
-      }
-    })
-  }
+  ngOnInit() {
+    this.getOrders();
+}
+getOrders() {
+  this.auth.User.subscribe(user => {
+    if(user){
+      this.ordersService.getorders(user.uid).subscribe(orders => this.orders = orders);
+      combineLatest(this.startObs, this.endObs).subscribe((value) => {
+        this.firequery(value[0], value[1], user.uid).subscribe((ordenes) => {
+          this.orders = ordenes;
+        })
+      })
+    }
+  })
+}
+firequery(start,end, uid){
+  return this.afs.collection('orders', ref => ref.orderBy('created_at').where("uid", "==", uid).where("created_at", ">=", start)).valueChanges();  
+}
 
 }
